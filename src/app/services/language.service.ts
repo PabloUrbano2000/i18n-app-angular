7ef547ca082a873c05e03e84509c5aa0598be83f
Pablo El Maldy <pablojamiro2008@hotmail.com>
2025-04-21 03:05:03 -0500

    feat

---
+++ src/app/services/language.service.ts
@@ -0,0 +1,17 @@
+import { inject, Injectable } from '@angular/core';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+
+  cookie = inject(SsrCookieService)
+
+  changeLang(lang: string){
+    this.cookie.set('lang', lang)
+
+    // TODO: Cambio de idioma
+  }
+
+}
